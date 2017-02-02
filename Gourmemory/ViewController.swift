5c11c1b6656b0c9f86b3a02e27ce50a80d3d1f97
小飼きわみ <kiwamikogai@me.com>
2017-02-02 20:00:38 +0900

    Initial Commit

---
+++ Gourmemory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gourmemory
+//
+//  Created by Kiwami on 2017/02/02.
+//  Copyright © 2017年 Kiwami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
